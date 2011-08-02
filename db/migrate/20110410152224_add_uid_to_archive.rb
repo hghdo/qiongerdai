8ebba96344faa4113920afe9d8ba4fbdf999842b
Gehua Huang <bolixie@gmail.com>
2011-08-02 16:15:38 +0800

    update jquery.js

--- db/migrate/20110410152224_add_uid_to_archive.rb
+++ db/migrate/20110410152224_add_uid_to_archive.rb
@@ -1,11 +1,9 @@
 class AddUidToArchive < ActiveRecord::Migration
   def self.up
     add_column :archives, :uid, :string
-    add_column :providers, :uid_pattern,:string
   end
 
   def self.down
     remove_column :archives, :uid
-    remove_column :providers, :uid_pattern
   end
 end
