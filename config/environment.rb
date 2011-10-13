4a89570286cdc39dc1300e730c8476b4b3791b10
Huang Gehua <bolixie@gmail.com>
2011-10-14 00:00:41 +0800

    modify logger to daily log file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the rails application
 Qiongerdai::Application.initialize!
+
+Rails.logger=Logger.new("#{Rails.root.to_s}/log/#{Rails.env}.log", "daily")
